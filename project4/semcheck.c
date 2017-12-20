aef6de287230a7e77106ae4c9c2d4a7772af81ba
stdio2016 <randomfrank8@gmail.com>
2017-12-20 09:30:52 +0800

    change error message

--- project4/semcheck.c
+++ project4/semcheck.c
@@ -8,12 +8,10 @@ void returnCheck(struct Expr *expr, struct Type *expected) {
     semanticError("program cannot be returned\n");
   }
   else if (expected->type == Type_VOID) {
-    semanticError("void function cannot be returned\n");
+    semanticError("procedure cannot be returned\n");
   }
   else if (expr->type == NULL) {
-    semanticError("return type is unknown, but '");
-    showType(expected);
-    printf("' is expected\n");
+    ; // don't show error
   }
   else if (!typeEqual(expr->type, expected)) {
     semanticError("return type mismatch, return type is '");
@@ -27,13 +25,16 @@ void returnCheck(struct Expr *expr, struct Type *expected) {
 
 void forCheck(int lowerBound, int upperBound) {
   if (lowerBound < 0 || upperBound < 0)
-    semanticError("loop parameter must be greater than or equal to zero\n");
+    semanticError("loop parameter is less than zero\n");
   if (lowerBound > upperBound)
-    semanticError("loop parameter must be in the incremental order\n");
+    semanticError("loop parameter is not in the incremental order\n");
 }
 
 void conditionCheck(struct Expr *expr, const char *ifwhile) {
-  if (expr->type == NULL || expr->type->type != Type_BOOLEAN) {
+  if (expr->type == NULL) {
+    ; // don't show error
+  }
+  else if (expr->type->type != Type_BOOLEAN) {
     semanticError("operand of %s statement is not boolean type\n", ifwhile);
     return;
   }
